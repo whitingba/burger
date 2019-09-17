6910730a08912abd0f8e53678048258722e78b16
Barbara Whiting <whitingba@gmail.com>
2019-09-17 13:28:18 -0600

    initial burgers_controllers.js file created

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,33 @@
+//Import Express
+var express = require('express');
+
+
+//Import burgers.js
+var burgers = require('../models/burger');
+
+//create the router
+var router = express.Router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
