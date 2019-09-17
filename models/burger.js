fc3ea97e4033a70b90f84c8665e0c90e279cd308
Barbara Whiting <whitingba@gmail.com>
2019-09-17 13:23:53 -0600

    create initial burger.js file

---
+++ models/burger.js
@@ -0,0 +1,31 @@
+var orm = require('../config/orm');
+
+
+
+
+
+
+
+
+
+
+
+
+// * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = burgers;
