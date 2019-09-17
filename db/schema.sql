a4ec9268c658865d0c6ed6868d8fbb791338de12
Barbara Whiting <whitingba@gmail.com>
2019-09-17 12:38:44 -0600

    initial files and SQL file created with initial burger_db and burgers table

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+    burger_name VARCHAR NOT NULL
+    (250),
+    devoured BOOLEAN
+    (10),
+    PRIMARY KEY
+    (id)
+);
