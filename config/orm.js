684165158f4bda0f9b3a3aea2d72475715caf09e
Barbara Whiting <whitingba@gmail.com>
2019-09-17 13:18:12 -0600

    initial orm.js file created

---
+++ config/orm.js
@@ -0,0 +1,36 @@
+//Import mysql connection
+var connection = require('../config/connection');
+
+
+
+
+
+
+// * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+// * `selectAll()`
+// * `insertOne()`
+// * `updateOne()`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = orm;
