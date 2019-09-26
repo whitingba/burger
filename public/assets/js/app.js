c07d70d40771422025652e95e1ded9d2086548cf
Barbara Whiting <whitingba@gmail.com>
2019-09-25 19:34:35 -0600

    devour a burger now functions

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,6 +2,24 @@
 //create a function that when you click on the devoured button the burger will move to the devoured column
 $(function () {
     //create a function that when you click on the devoured button the burger will move to the devoured column
+    $('.devour').on('click', function (event) {
+        event.preventDefault();
+
+        var id = $(this).data('id');
+        var devouredState = {
+            devoured: 1
+        };
+        //Send the PUT request
+        $.ajax('/api/burgers/' + id, {
+            type: 'PUT',
+            data: devouredState
+        }).then(function () {
+            console.log('Burger devoured');
+            location.reload();
+        })
+    })
+
+
 
 
 
